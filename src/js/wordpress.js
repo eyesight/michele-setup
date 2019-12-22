9685603181857c518ae7da6f97592b69cfc36aac
Claudia Friedrich <claudae_f@hotmail.com>
2019-12-22 11:44:19 +0100

    cleaned up

--- src/js/wordpress.js
+++ src/js/wordpress.js
@@ -1,13 +1,10 @@
 // Bundle theme module
-import LazyLoad from './lib/lazyload';
 import Menu from './Classes/Menu';
 import CategoryLoader from './Classes/CategoryLoader';
-import VideoPlay from './Classes/VideoPlay';
-import PostTitleLoader from './Classes/PostTitleLoader';
 
 (function() {
   document.addEventListener('DOMContentLoaded', function() {
-		new LazyLoad();
+		//new LazyLoad();
 	const navigation = new Menu();
 	
   const categoriesLoader = new CategoryLoader();
