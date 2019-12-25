fe409ff84df4e937e69bed1fdc782f4c36212012
Claudia Friedrich <claudae_f@hotmail.com>
2019-12-25 10:18:51 +0100

    deleted mobile navigation css and js

--- src/js/wordpress.js
+++ src/js/wordpress.js
@@ -1,11 +1,9 @@
 // Bundle theme module
-import Menu from './Classes/Menu';
 import CategoryLoader from './Classes/CategoryLoader';
 
 (function() {
   document.addEventListener('DOMContentLoaded', function() {
 		//new LazyLoad();
-	const navigation = new Menu();
 	
   const categoriesLoader = new CategoryLoader();
 
