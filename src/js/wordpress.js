611746971d113a2ec1871ecfce63ff25cffb1b0a
Claudia Friedrich <claudae_f@hotmail.com>
2024-03-18 22:46:08 +0100

    update styling

--- src/js/wordpress.js
+++ src/js/wordpress.js
@@ -1,16 +1,12 @@
 // Bundle theme module
 import CategoryLoader from './Classes/CategoryLoader';
 import ToTop from './Classes/ToTop';
-import LogoScroll from './Classes/LogoScroll';
-import TickerAni from './Classes/TickerAni';
 
 (function() {
   document.addEventListener('DOMContentLoaded', function() {
 	
   const categoriesLoader = new CategoryLoader();
   const toTop = new ToTop();
-  const logoScroll = new LogoScroll();
-  const tickerAni = new TickerAni();
 
   });
 })();   
