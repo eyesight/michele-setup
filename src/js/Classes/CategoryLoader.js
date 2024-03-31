b17f83e6b2a46839da01ce7206182e4167328644
Claudia Friedrich <claudae_f@hotmail.com>
2024-03-31 14:41:51 +0200

    update category loader

--- src/js/Classes/CategoryLoader.js
+++ src/js/Classes/CategoryLoader.js
@@ -3,8 +3,13 @@ import Helper from './Helper';
 class CategoryLoader {
   constructor() {
     this.filterButtonSelector = '.filter__button';
-    this.buttonClicket = document.querySelectorAll('.filter__button');
-    this.buttonClicketArray = Array.prototype.slice.call(this.buttonClicket);
+    this.buttonClicked = document.querySelectorAll('.filter__button');
+    this.buttonClickedSmall = document.querySelectorAll('.tiles__cat-filter');
+    this.buttonClickedArray = Array.prototype.slice.call(this.buttonClicked);
+    this.buttonClickedSmallArray = Array.prototype.slice.call(this.buttonClickedSmall);
+    // Combine the arrays
+    this.combinedButtonArray = this.buttonClickedArray.concat(this.buttonClickedSmallArray);
+
     this.bodyEl = document.querySelector('body');
     
     this.categoryPage = document.querySelector('.category');
@@ -16,7 +21,7 @@ class CategoryLoader {
     
     this.filterArr = [];
     this.timeOutTime = 300;
-    if(this.buttonClicket && this.buttonClicket.length > 0){
+    if(this.buttonClicked && this.buttonClicked.length > 0){
       this.bindEvents();
     }
   }
@@ -24,20 +29,20 @@ class CategoryLoader {
   bindEvents() {
     //if user is on category-page, show only the elements of the category in the beginning
     if(this.categoryPage){
-      let cat = this.getCurrentCategory(this.buttonClicketArray);
+      let cat = this.getCurrentCategory(this.buttonClickedArray);
       this.togglePresent(this.filterArr, cat);
       this.hideItems(this.filteredItemsArray, this.filterArr);
     }
     
     this.showAllitems();
-    this.buttonClicketArray.forEach(button => {
+    this.combinedButtonArray.forEach(button => {
       button.addEventListener('click', e => {
         e.preventDefault();
         const clickedButton = e.currentTarget;
         let currentCategory = clickedButton.getAttribute('data-filter-target');
-        this.setActiveButton(clickedButton); 
-        this.animateFilteredItems(this.filterArr);
+        this.setActiveButton(clickedButton, this.combinedButtonArray); 
         this.togglePresent(this.filterArr, currentCategory);
+        this.animateFilteredItems(this.filterArr);
         setTimeout(()=>{
           this.hideItems(this.filteredItemsArray, this.filterArr);
         }, this.timeOutTime);
@@ -65,11 +70,23 @@ class CategoryLoader {
     });
   }
 
-  setActiveButton(clickedButton){ 
+  setActiveButton(clickedButton, allButtons){ 
+    // If clicked btn has class active, then all btns with the same category will the active class be removed
+    // if it is not active yet, all btn with the same category get the class active
+    const categoryOfClickedBtn = clickedButton.getAttribute('data-filter-target');
+    
     if(clickedButton.classList.contains('active')){
-      Helper.removeClass(clickedButton, 'active')
-    }else{
-      Helper.addClass(clickedButton, 'active');
+      for (let i = 0; i < allButtons.length; i++) {
+        if (categoryOfClickedBtn === allButtons[i].getAttribute('data-filter-target')) {
+            Helper.removeClass(allButtons[i], 'active');
+        }
+      }
+    } else {
+      for (let i = 0; i < allButtons.length; i++) {
+        if (categoryOfClickedBtn === allButtons[i].getAttribute('data-filter-target')) {
+            Helper.addClass(allButtons[i], 'active');
+        }
+      }
     }
   }
   
@@ -103,7 +120,7 @@ class CategoryLoader {
     let categoryName = '';
     buttons.forEach((name)=>{
         if(name.classList.contains('active')){
-          categoryName = name.getAttribute('data-filter-target')
+          categoryName = name.getAttribute('data-filter-target');
         }
     });
     return categoryName;
